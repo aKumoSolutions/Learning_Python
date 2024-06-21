492b15e706a256c9cc2ca833b6265a94bc047271
copypasteninja <abdugofir00@gmail.com>
2024-06-21 02:10:04 +0000

    Python 101 - session 1

---
+++ Python101/interactive.py
@@ -0,0 +1,9 @@
+
+name = input("What is your name?: ")
+name_length = len(name)
+
+# print("Hello there", name, "Your name is ", name_length, "characters long!")
+# print(f"Hello there {name}! Your name is {name_length} characters long!")
+
+string = "hello"
+print(string * str(5))
---
+++ Python101/math.py
@@ -0,0 +1,11 @@
+## Math Operations
+
+addition = 4 + 4
+subtraction = 5 - 2
+multiplication = 5 * 5
+division = 12 / 6
+int_division = 8 // 2
+modulus_division = 18 % 6
+power_sum = 2 ** 3
+
+print(power_sum)
