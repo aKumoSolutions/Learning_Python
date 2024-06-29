1c7bab160439e02e198003444bcf5c17ba9a823e
copypasteninja <abdugofir00@gmail.com>
2024-06-29 22:16:39 +0000

    session 9 code

--- Python101/sort.py
+++ Python101/sort.py
@@ -1,6 +1,9 @@
 num = [1, 2, 4, 6, 9, 8, 10, 3]
 ## sorts the original (num) list
-num.sort()
+# num.sort()
 
 ## saves a sorted version of the (num) list
-copy = sorted(num)
+copied = sorted(num)
+copied.append(5)
+copied.sort(reverse=True)
+print(copied)
