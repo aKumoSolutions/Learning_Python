6f234aa1bfc6cb8152a1669b06bbc4380a2cf404
zarifi <iqbalzarifi@icloud.com>
2024-10-19 18:18:50 +0000

    saturday

--- august/iqbal/control.py
+++ august/iqbal/control.py
@@ -1,26 +1,35 @@
-# for i in range(1, 10):
-#     if i == 10:
-#         print(i + 2)
-#         continue ## break
-#     print(i + 1)
+# # for i in range(1, 10):
+# #     if i == 10:
+# #         print(i + 2)
+# #         continue ## break
+# #     print(i + 1)
 
 
-fruits = ['apple', 'banana', 'cherry', "carrot"]
-removed = 0
-for item in fruits:
-    if item == "carrot":
-        fruits.remove(item)
-        removed += 1
-        continue
 
-    elif item == "potato":
-        pass
-    print(item, "is a great fruit")
-print(fruits)
-print(removed, "# of carrots removed")
+# # fruits = ['apple', 'banana', 'cherry', "carrot"]
+# removed = 0
+# for item in fruits:
+#     if item == "carrot":
+#         fruits.remove(item)
+#         removed += 1
+#         continue
 
-# if 'pear' or 'banana' in fruits:
+#     elif item == "potato":
+#         pass
+#     print(item, "is a great fruit")
+# print(fruits)
+# print(removed, "# of carrots removed")
 
-#     print("yes")
-# else:
-#     print("no")
+
+
+# # if 'pear' or 'banana' in fruits:
+
+# #     print("yes")
+# # else:
+# #     print("no")
+
+for i in range(3):
+    for j in range(3):
+        print("*", end=" ")
+    print(" ")
+    
