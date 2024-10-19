63fa73aba42c06677e4d7530aabbdf68e4d96f80
zarifi <iqbalzarifi@icloud.com>
2024-10-19 17:46:36 +0000

    this is saturday's commit

---
+++ august/iqbal/control.py
@@ -0,0 +1,26 @@
+# for i in range(1, 10):
+#     if i == 10:
+#         print(i + 2)
+#         continue ## break
+#     print(i + 1)
+
+
+fruits = ['apple', 'banana', 'cherry', "carrot"]
+removed = 0
+for item in fruits:
+    if item == "carrot":
+        fruits.remove(item)
+        removed += 1
+        continue
+
+    elif item == "potato":
+        pass
+    print(item, "is a great fruit")
+print(fruits)
+print(removed, "# of carrots removed")
+
+# if 'pear' or 'banana' in fruits:
+
+#     print("yes")
+# else:
+#     print("no")
---
+++ august/iqbal/loops.py
@@ -0,0 +1,21 @@
+# range
+# for num in range(5):
+    # print(num)
+
+
+    # list
+
+    # names = ["iqbal", "akmal", "abdul"]
+    # for name in names:
+    #     print(name)
+
+    # ## string
+    # for letter in "python":
+    #     print(letter)
+
+    ## while loop
+
+    i = 1
+    while i <= 5:
+        print(i)
+        i += 1 # i = i + 1
